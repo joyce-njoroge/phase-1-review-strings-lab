ec488af54d7ee053d5990bf449744f8c03209b64
Joyce Njoroge <joycewambuku33@gmail.com>
2023-03-16 12:40:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Eren Jaeger';
+const welcomeMessage = 'Welcome to Flatbook, ${Eren Jaeger}!';
+ const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${WELCOME TO FLATBOOK}, ${EREN JAEGER}!';
+ const shortGreeting = "Welcome, ${Welcome}, ${E}!";
+
+
+
